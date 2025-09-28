0c84b5f293293052cb1d02c0bbe12c4e6995463c
Delta student <149225920+Tushar123097@users.noreply.github.com>
2025-09-28 23:41:29 +0530

    Use Atlas connection string for deployment

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB connection
+
+//MongoDB connection
 mongoose
-  .connect("mongodb://127.0.0.1:27017/myNewDatabase", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
