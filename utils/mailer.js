aca07201ae239647a2ef2c44eeb0418f606575d9
Delta student <149225920+Tushar123097@users.noreply.github.com>
2025-09-30 18:57:47 +0530

    updated

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,18 +1,41 @@
-const transporter = require("./nodemailerTransporter");
+// const transporter = require("./nodemailerTransporter");
+
+// const sendEmail = async (to, subject, text, html) => {
+//   try {
+//     await transporter.sendMail({
+//       from: process.env.EMAIL_USER,
+//       to,
+//       subject,
+//       text,
+//       html,
+//     });
+//     console.log("Email sent to", to);
+//   } catch (err) {
+//     console.error("Email failed:", err);
+//   }
+// };
+
+// module.exports = sendEmail;
+// utils/sendEmail.js
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (to, subject, text, html) => {
+  const msg = {
+    to,
+    from: "prajapatit097@gmail.com", // must be verified in SendGrid
+    subject,
+    text,
+    html,
+  };
+
   try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to,
-      subject,
-      text,
-      html,
-    });
+    await sgMail.send(msg);
     console.log("Email sent to", to);
   } catch (err) {
-    console.error("Email failed:", err);
+    console.error("SendGrid error:", err.response?.body || err);
   }
 };
 
 module.exports = sendEmail;
+
